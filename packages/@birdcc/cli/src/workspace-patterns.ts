4b1c0f923954d679278d543f3789be1e4caeb2e8
Alice39s <alice39s@xmsl.dev>
2026-03-06 22:05:28 +0900

    fix(workspace): Ignore symbolic links

--- packages/@birdcc/cli/src/workspace-patterns.ts
+++ packages/@birdcc/cli/src/workspace-patterns.ts
@@ -113,6 +113,9 @@ const listWorkspaceCandidateDirectories = async (
     }
 
     for (const entry of entries) {
+      if (entry.isSymbolicLink()) {
+        continue;
+      }
       if (!entry.isDirectory()) {
         continue;
       }
